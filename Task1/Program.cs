94849e82304db29b608568fdfa91cdfc532f5a4f
Natalie <sonatalex@gmail.com>
2024-07-04 21:55:38 +0300

    1 task is done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("тест");
+﻿// Программа, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Введи первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введи второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    System.Console.Write($"{firstNumber} больше, чем {secondNumber}");
+}
+if (firstNumber < secondNumber)
+{
+    System.Console.Write($"{secondNumber} больше, чем {firstNumber}");
+}
+if (firstNumber == secondNumber)
+{
+    System.Console.Write($"{secondNumber} равно {firstNumber}");
+}
