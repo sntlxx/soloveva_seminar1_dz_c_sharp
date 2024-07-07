5c956ac5a5fc16a59a1edaaa1769e1c5cd257e53
Natalie <sonatalex@gmail.com>
2024-07-07 15:11:18 +0300

    Task 4 is done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("1 "); 
+
+for(int i = 2; i < number; i += 2) 
+{
+ Console.Write(i + " ");
+}
+
+if (number != 1) 
+{
+ Console.Write(number);
+}
