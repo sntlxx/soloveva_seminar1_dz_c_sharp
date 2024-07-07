ff58ee76a804859e89fd1e68f382d59dd3423a3b
Natalie <sonatalex@gmail.com>
2024-07-07 14:40:04 +0300

    second task is done

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("THIS PROGRAM WILL HELP YOU TO FIND THE BIGGEST NUMBER");
+System.Console.WriteLine("Type the first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Type the second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Type the third number");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int maximum = firstNumber;
+if (maximum < secondNumber)
+{
+    maximum = secondNumber;
+}
+if (maximum < thirdNumber)
+{
+    maximum = thirdNumber;
+}
+System.Console.WriteLine ($" {maximum} is the biggest number");
