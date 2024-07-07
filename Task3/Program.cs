4c7a960d8176d9c8b97ecfa06ee83e44bcfbe7be
Natalie <sonatalex@gmail.com>
2024-07-07 14:48:14 +0300

    initial commit 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($" {number} является четным");
+}
+else
+{
+    System.Console.WriteLine($" {number} является нечетным");
+}
